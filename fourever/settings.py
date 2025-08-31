e25ec4b6fa0dd56580869434b4705616e8e67406
Elmer Gonzalez <elmergonzalez712@gmail.com>
2025-08-31 18:54:12 -0500

    setting change

--- fourever/settings.py
+++ fourever/settings.py
@@ -133,6 +133,9 @@ CACHES = {
     }
 }
 
+CELERY_BROKER_URL = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/0")
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
